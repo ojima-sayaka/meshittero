c02187dbc0ed636abfc913940fbef5febc69e913
EC2 Default User <ec2-user@ip-172-31-17-194.ec2.internal>
2022-02-16 11:01:27 +0000

    ここまでは問題なく　動く

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  root 'homes#top'
   devise_for :users
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
